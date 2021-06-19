e54be1b613a2d48e0c10c72b9f0477f8649bff14
Martina <martinaescorial9@gmail.com>
2021-06-19 07:49:16 +0200

    Add project files.

---
+++ U4E7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace U4E7
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            int x;
+            for (x = 1; x <=100; x++)
+            {
+                Console.Write(x);
+                Console.Write("-");
+            }
+            Console.ReadKey();
+        }
+    }
+}
